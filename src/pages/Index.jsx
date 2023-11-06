90ca5764c4179e5b44badafc6bfbe3d3bb94b0f6
lisamarimyrene <lisamy@stud.ntnu.no>
2023-11-06 09:33:52 +0100

    local storage fixes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,92 +1,63 @@
-import { Text, View, FlatList, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
-// import { Svg, Path } from 'react-native-svg'
+import { View, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
 import { PostIt } from '../components/PostIt'
+import { NewNote } from '../components/NewNote';
 import { colors } from '../../themes/colors'
 import Svg, { Path } from 'react-native-svg';
-import NewNoteIcon from '../../assets/newIcon.svg'
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const testPosts = [
-    {
-        id: 1,
-        color: "yellow",
-        title: "Mitt første notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 2,
-        color: "pink",
-        title: "Mitt andre notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 3,
-        color: "blue",
-        title: "Mitt tredje notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 4,
-        color: "green",
-        title: "Mitt fjerde notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 5,
-        color: "yellow",
-        title: "Mitt første notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 6,
-        color: "pink",
-        title: "Mitt andre notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 7,
-        color: "blue",
-        title: "Mitt tredje notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 8,
-        color: "green",
-        title: "Mitt fjerde notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 9,
-        color: "green",
-        title: "Mitt fjerde notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 10,
-        color: "yellow",
-        title: "Mitt første notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 11,
-        color: "pink",
-        title: "Mitt andre notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 12,
-        color: "blue",
-        title: "Mitt tredje notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    },
-    {
-        id: 13,
-        color: "green",
-        title: "Mitt fjerde notat",
-        content: "Heihei! Jeg tester den nye appen her..."
-    }
-]
 
 export const Index = () => {
+    const [showNewNote, setShowNewNote] = useState(false);
+    const [notes, setNotes] = useState([]);
+
+    // Load notes
+    useEffect(() => {
+        // Load notes from AsyncStorage
+        AsyncStorage.getItem('notes')
+            .then(savedNotes => {
+                if (savedNotes) {
+                    const parsedNotes = JSON.parse(savedNotes);
+                    setNotes(parsedNotes);
+                }
+            })
+            .catch(error => {
+                console.error('Error loading notes from AsyncStorage: ', error);
+            });
+    }, []);
+
+    // Function to generate the next id
+    const getNewId = () => {
+        const newId = Date.now(); // Use a timestamp as a unique ID
+        return newId.toString();
+    };
+
+    // Save new note
+    const handleSaveNote = newNote => {
+        const newId = getNewId(); // Assign a unique ID to the note
+        newNote.id = newId;
+    
+        // Update the notes array
+        const updatedNotes = [...notes, newNote];
+    
+        // Store the updated notes in AsyncStorage
+        AsyncStorage.setItem('notes', JSON.stringify(updatedNotes))
+            .then(() => {
+                // Update the state with the new notes
+                setNotes(updatedNotes);
+                setShowNewNote(false);
+            })
+            .catch(error => {
+                console.error('Error saving notes to AsyncStorage: ', error);
+            });
+    }
+
+
+    // Do not save new note
+    const handleCancel = () => {
+        setShowNewNote(false);
+    };
+
     return (
         <View style={styles.main}>
             <ScrollView style={styles.scrollContainer}
@@ -94,21 +65,28 @@ export const Index = () => {
                 showsHorizontalScrollIndicator={false}
             >
                 <View style={styles.postitSection}>
-                    {testPosts.map((post) => (
+                    {notes.map((note) => (
                         <PostIt
-                            key={post.id}
-                            color={post.color}
-                            title={post.title}
-                            content={post.content}
+                            key={note.id}
+                            color={note.color}
+                            title={note.title}
+                            content={note.content}
                         />
                     ))}
                 </View>
             </ScrollView>
-            <TouchableOpacity style={styles.buttonContainer}>
-                <Svg width="90" height="90" viewBox="0 0 162 162" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <Path d="M81 16.2C45.2142 16.2 16.2 45.2142 16.2 81C16.2 116.786 45.2142 145.8 81 145.8C116.786 145.8 145.8 116.786 145.8 81C145.8 45.2142 116.786 16.2 81 16.2ZM113.4 86.4H86.4V113.4C86.4 116.386 83.9808 118.8 81 118.8C78.0192 118.8 75.6 116.386 75.6 113.4V86.4H48.6C45.6192 86.4 43.2 83.9862 43.2 81C43.2 78.0138 45.6192 75.6 48.6 75.6H75.6V48.6C75.6 45.6138 78.0192 43.2 81 43.2C83.9808 43.2 86.4 45.6138 86.4 48.6V75.6H113.4C116.381 75.6 118.8 78.0138 118.8 81C118.8 83.9862 116.381 86.4 113.4 86.4Z" fill="#B17A9B" />
-                </Svg>
-            </TouchableOpacity>
+            {showNewNote ? (
+                <NewNote onSave={handleSaveNote} onCancel={handleCancel} noteId={getNewId} />
+            ) : (
+                <TouchableOpacity
+                    style={styles.buttonContainer}
+                    onPress={() => setShowNewNote(true)}
+                >
+                    <Svg width="90" height="90" viewBox="0 0 162 162" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <Path d="M81 16.2C45.2142 16.2 16.2 45.2142 16.2 81C16.2 116.786 45.2142 145.8 81 145.8C116.786 145.8 145.8 116.786 145.8 81C145.8 45.2142 116.786 16.2 81 16.2ZM113.4 86.4H86.4V113.4C86.4 116.386 83.9808 118.8 81 118.8C78.0192 118.8 75.6 116.386 75.6 113.4V86.4H48.6C45.6192 86.4 43.2 83.9862 43.2 81C43.2 78.0138 45.6192 75.6 48.6 75.6H75.6V48.6C75.6 45.6138 78.0192 43.2 81 43.2C83.9808 43.2 86.4 45.6138 86.4 48.6V75.6H113.4C116.381 75.6 118.8 78.0138 118.8 81C118.8 83.9862 116.381 86.4 113.4 86.4Z" fill="#B17A9B" />
+                    </Svg>
+                </TouchableOpacity>
+            )}
         </View>
     )
 }
