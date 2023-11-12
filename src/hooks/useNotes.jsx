73d1e18e740df5d7d7697c50bd03f8f34981d990
lisamarimyrene <lisamy@stud.ntnu.no>
2023-11-12 20:14:29 +0100

    refactor: changed function names

--- src/hooks/useNotes.jsx
+++ src/hooks/useNotes.jsx
@@ -7,10 +7,12 @@ export const useNotes = () => {
     const [refreshTrigger, setRefreshTrigger] = useState(false)
     const [notes, setNotes] = useState([])
 
+    // Trigger refresh of data function
     const triggerRefresh = () => {
         setRefreshTrigger(!refreshTrigger)
     }
 
+    // Refresh notes data that are listening on the refreshTrigger state
     useEffect(() => {
         AsyncStorage.getItem('notes')
         .then(savedNotes => {
@@ -30,23 +32,29 @@ export const useNotes = () => {
     const handleSaveNote = (noteToSave) => {
         let newNotesArray;
 
-        const noteExists = notes.filter((note) => {
-            if (noteToSave.id == note.id) return true
+        const noteIdExists = notes.filter((note) => {
+            if (noteToSave.id === note.id) return note
+            console.log(note);
         })
+        console.log("noteExists", noteIdExists);
 
-        if (noteExists.length) {
+        if (noteIdExists.length) {
             // If you are editing a note, update it in the existing notes
             newNotesArray = notes.map((note) =>
                 note.id === noteToSave.id ? { ...note, ...noteToSave } : note
             );
-
+            console.log("if", noteToSave.id);
+            
+            
             // ? Remove
             // Reset input fields to null
             // setEditNote(null);
+            //! kommer ikke inn til elsen
         } else {
             // If you are adding a new note, add it to the existing notes
             noteToSave.id = getNewId();
             newNotesArray = [...notes, noteToSave];
+            console.log("else", noteToSave.id);
         }
 
         overwriteNotes(newNotesArray)
