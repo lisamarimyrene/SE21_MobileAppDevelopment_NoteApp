273d747e719962cebd6c1f0a242e5505b8686994
lisamarimyrene <lisamy@stud.ntnu.no>
2023-11-14 16:05:42 +0100

    refactor: put getItem function in asyncStorage.js

--- src/hooks/useNotes.jsx
+++ src/hooks/useNotes.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { updateNotesArray } from "../utils/asyncStorage";
+import { getAllNotes, updateNotesArray } from "../utils/asyncStorage";
 import { generateNewId } from "../utils/generateNewId";
 import { router } from 'expo-router';
 
@@ -9,22 +9,11 @@ export const useNotes = () => {
 
     // Refresh notes data that are listening on the refreshTrigger state
     useEffect(() => {
-        AsyncStorage.getItem('notes')
-            .then(savedNotes => {
-                if (savedNotes) {
-                    const parsedNotes = JSON.parse(savedNotes);
-                    setNotes(parsedNotes)
-                }
-            })
-            // Error message in case it's not able to load notes
-            .catch(error => {
-                console.error('Error loading notes from AsyncStorage: ', error);
-            });
-
+        getAllNotes(setNotes);
     }, [])
 
     // Save note created or edited
-    const handleSaveNote = async ( title, content, color, imageUri ) => {
+    const handleSaveNote = (title, content, color, imageUri) => {
 
         let newNotesArray;
 
@@ -45,61 +34,41 @@ export const useNotes = () => {
 
     }
 
-    // const handleSave = async () => {
-    //     if (title || content) {
-    //         const newNote = {
-    //             id: {id},
-    //             color: color,
-    //             title: title,
-    //             content: content,
-    //             image: imageUri,
-    //         };
-
-    //         handleCancel();
-
-    //     }
-    //     else {
-    //         Alert.alert(
-    //             'Did you mean to save?',
-    //             'Please enter some input or delete the note.',
-    //             [
-    //                 {
-    //                     text: 'OK',
-    //                 },
-    //             ],
-    //             { cancelable: true }
-    //         );
-    //     }
-    // };
-
-
-
-    // Handle delete note
-    const handleDeleteNote = (noteToDelete) => {
+    // Delete selected note
+    const handleDeleteNote = (id) => {
         let newNotesArray;
-
-        // newNotesArray = notes.map((note) =>
-        //     note.id === noteToDelete.id ? { ...note, ...noteToDelete } : note
-        // );
+        let noteToDelete;// Må finne notatet jeg klikker på
 
         newNotesArray = notes.filter((note) => {
-            if (note.id !== noteToDelete.id) {
+            if (note.id === noteToDelete.id) {
                 return note
             }
         })
 
-        AsyncStorage.setItem(
-            'notes',
-            JSON.stringify(newNotesArray)
-        )
-            .then(() => {
-                // Dismiss the editing view
-                overwriteNotes(newNotesArray)
-                toggleModal(false)
-            })
-            .catch((error) => {
-                console.error('Error saving notes to AsyncStorage: ', error);
-            });
+        Alert.alert(
+            'Confirm Delete',
+            'Are you sure you want to delete this note?',
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Delete',
+                    onPress: () => {
+                        onCancel(true);
+                        router.back()
+                    },
+                },
+            ],
+            { cancelable: false }
+        );
+
+        newNotesArray = [...notes, newNote];
+        console.log(newNotesArray);
+
+        updateNotesArray(newNotesArray, setNotes)
+
 
     }
 
--- src/utils/asyncStorage.js
+++ src/utils/asyncStorage.js
@@ -1,17 +1,31 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// Update notes array saved in AsyncStorage
 export const updateNotesArray = async (newNoteArray, setNotes) => {
 
     // Store the updated notes in AsyncStorage
     await AsyncStorage.setItem('notes', JSON.stringify(newNoteArray))
-    .then(() => {
-        // todo: create toggle modal
-
-        console.log('saved new notes')
-        setNotes(newNoteArray);
-
-    })
-    .catch((error) => {
-        console.error('Error saving notes to AsyncStorage: ', error);
-    });
+        .then(() => {
+            setNotes(newNoteArray);
+        })
+        .catch((error) => {
+            console.error('Error saving notes to AsyncStorage: ', error);
+        });
 }
+
+// Get all notes saved in AsyncStorage
+export const getAllNotes = async (setNotes) => {
+
+    AsyncStorage.getItem('notes')
+        .then(savedNotes => {
+            if (savedNotes) {
+                const parsedNotes = JSON.parse(savedNotes);
+                setNotes(parsedNotes)
+            }
+        })
+        // Error message in case it's not able to load notes
+        .catch(error => {
+            console.error('Error loading notes from AsyncStorage: ', error);
+        });
+}
+
