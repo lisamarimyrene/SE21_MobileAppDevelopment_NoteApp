c8855f84703e2a57a61939d4e27e5532fc25fcbd
lisamarimyrene <lisamy@stud.ntnu.no>
2023-11-14 17:24:31 +0100

    staged

--- src/utils/generateNewId.js
+++
@@ -1,6 +0,0 @@
-// Function to generate the an unique id for the postit notes
-//* Endre til uuid(?)
-export const generateNewId = () => {
-    const newId = Date.now();
-    return newId.toString();
-};
