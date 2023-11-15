b1bdc97c41d410bb6da5f1668536ceb6b45c2404
lisamarimyrene <lisamy@stud.ntnu.no>
2023-11-15 16:45:31 +0100

    test: generateNewId test

---
+++ src/tests/generateNewId.test.js
@@ -0,0 +1,18 @@
+import { generateNewId } from '../utils/generateNewId';
+
+// Test for generateNewId function
+test('test if it generates a new id', () => {
+    // Call the function to generate a new ID
+    const newId = generateNewId();
+
+    // Expect the newId to be defined and a string
+    expect(newId).toBeDefined();
+    expect(typeof newId).toBe('string');
+
+    // Convert the newId back to a number
+    const parsedId = parseInt(newId, 10);
+
+    // Expect the parsedId to be a number and not NaN
+    expect(typeof parsedId).toBe('number');
+    expect(!isNaN(parsedId)).toBe(true);
+});
